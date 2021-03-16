6de04164679fe5476ceae571a4305d6ecbea5b9a
Philippe PERECHODOV <philippe.perechodov@free.fr>
2021-03-16 21:37:19 +0100

    Incorporez du style et des assets à votre projet

--- src/components/App.js
+++ src/components/App.js
@@ -5,22 +5,9 @@ import Cart from './Cart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        <Banner />
-        <Cart />
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Banner />
+      <Cart />
     </div>
   );
 }
